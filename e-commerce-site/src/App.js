961b14c5a05c550bfa7363f09c15e20250c9f0b7
bcbeltran <benjamin.beltran37@gmail.com>
2022-02-06 21:26:28 -0700

    Began routing using react router dom and enabled navigating to shopping pages by clicking on the item you're interested in

--- e-commerce-site/src/App.js
+++ e-commerce-site/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Routes } from 'react-router-dom';
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+    <Routes>
+      <Route exact path='/' element={<HomePage />} />
+      <Route path='/shop/hats' element={<HatsPage />} />
+    </Routes>
     </div>
   );
 }
