74a1ef69d73503cc4669caa8a33857a90309ab6e
bcbeltran <benjamin.beltran37@gmail.com>
2022-02-11 18:03:00 -0700

    Added a cart icon and cart dropdown functionality when clicking the cart icon

--- e-commerce-site/src/components/header/Header.js
+++ e-commerce-site/src/components/header/Header.js
@@ -3,10 +3,12 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/xrp-logo.svg';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/CartIcon';
+import CartDropdown from '../cart-dropdown/CartDropdown';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
     return (
 		<div className="header">
 			<Link className="logo-container" to="/">
@@ -30,13 +32,16 @@ const Header = ({ currentUser }) => {
 						SIGN IN
 					</Link>
 				)}
+				<CartIcon />
 			</div>
+			{hidden ? null : <CartDropdown />}
 		</div>
 	);
 }
 
-const mapStateToProps = state => ({
-	currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+	currentUser,
+	hidden
 });
 
 export default connect(mapStateToProps)(Header);
